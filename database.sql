0beafc1ac6b7c707467062d4ae70c6a040001168
IvanFoksha <v.foksh6@gmail.com>
2025-07-22 19:40:21 +0200

    Update database.sql - added new columns

--- database.sql
+++ database.sql
@@ -1,5 +1,7 @@
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    last_checked TIMESTAMP,
+    status_code INTEGER
 );
