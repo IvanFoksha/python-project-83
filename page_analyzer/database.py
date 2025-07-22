48a6b7b720f7b4456339968ee04fcf049d2ad53b
IvanFoksha <v.foksh6@gmail.com>
2025-07-22 18:52:53 +0200

    Update database - create function add_url

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -7,3 +7,37 @@ import validators
 
 def get_db_connection():
     return psycopg2.connect(os.getenv('DATABASE_URL'))
+
+
+def add_url(url):
+    parser = urlparse(url)
+    normalized_url = f'{parser.scheme}://{parser.netloc}'
+
+    if not validators.url(normalized_url):
+        return None, 'Некоректный URL'
+    if len(normalized_url) > 255:
+        return None, 'URL превышает 255 символов'
+    
+    with get_db_connection as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                'SELECT id FROM urls WHERE name = %s',
+                (normalized_url,)
+            )
+            existing = cur.fetchone()
+            if existing:
+                return existing[0], None
+
+            cur.execute(
+                """
+                INSERT INTO urls (name, created_at)
+                VALUES (%s, %s)
+                RETURING id
+                """,
+                (normalized_url, datetime.now())
+            )
+            url_id = cur.fetchone()[0]
+            conn.commit()
+            return url_id, None
+        
+
