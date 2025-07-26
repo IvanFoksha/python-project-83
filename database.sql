f866b0c9918479bdeeb001fdde5bab01292afbfa
IvanFoksha <v.foksh6@gmail.com>
2025-07-26 10:01:24 +0200

    Update database - added table with checks for site

--- database.sql
+++ database.sql
@@ -5,3 +5,15 @@ CREATE TABLE urls (
     last_checked TIMESTAMP,
     status_code INTEGER
 );
+
+
+CREATE TABLE checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL,
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at  TIMESTAMP NOT NULL,
+    FOREIGN KEY (url_id) REFERENCES urls(id) ON DELETE CASCADE
+);
