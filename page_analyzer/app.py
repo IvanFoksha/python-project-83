c7d5da241f272e7ec289f024de92ddbecf76bc4c
IvanFoksha <v.foksh6@gmail.com>
2025-07-26 15:40:38 +0200

    Update app.py to separate logic

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from dotenv import load_dotenv
-from page_analyzer.database import (
-    add_url,
-    get_url_by_id,
-    get_all_urls,
-    add_check
-)
+from page_analyzer import database, checker, validator
 
 load_dotenv()
 app = Flask(__name__)
@@ -21,29 +16,35 @@ def index():
 @app.route('/urls', methods=['GET', 'POST'])
 def urls():
     if request.method == 'POST':
-        url = request.form.get('url')
-        url_id, error = add_url(url)
+        url_input = request.form.get('url')
+        if not validator.is_valid_url(url_input):
+            error = 'Некорректный URL' if url_input else 'Заполните это поле.'
+            flash(error, 'danger')
+            return render_template('index.html'), 422
+        parsed_url = validator.normalize_url(url_input)
+        url_id, error = database.add_url(parsed_url)
         if error:
             flash(error, 'danger')
             return render_template('index.html'), 422
         flash('Страница успешно добавлена', 'seccess')
         return redirect(url_for('url_detail', id=url_id))
 
-    urls = get_all_urls()
+    urls = database.get_all_urls()
     return render_template('urls.html', urls=urls)
 
 
 @app.route('/urls/<int:id>', methods=['GET', 'POST'])
 def url_detail(id):
-    url = get_url_by_id(id)
+    url = database.get_url_by_id(id)
     if not url:
         flash('URL не найден', 'danger')
         return redirect(url_for('urls'))
     if request.method == 'POST':
-        status_code = None
-        h1 = None
-        title = None
-        description = None
-        add_check()
-    
+        try:
+            status_code, h1, title, description = checker.check_url(url[1])
+            database.add_check(id, status_code, h1, title, description)
+            flash('Страница успешно проверена', 'success')
+        except checker.CheckError:
+            flash('Ошибка проверки: не удалось подключиться к сайту', 'danger')
+        return redirect(url_for('url_detail', id=id))
     return render_template('url_detail.html', url=url)
