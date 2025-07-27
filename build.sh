6b73e4f91505e38bee93b5cb5462d5e38e560ae5
IvanFoksha <v.foksh6@gmail.com>
2025-07-27 13:51:01 +0200

    Fix build.sh to assembly in render

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-# Скачиваем uv и запускаем команду установки зависимостей
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install
