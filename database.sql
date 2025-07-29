e5c87c3edea3c068ef8b2da4e06286b948b81858
IvanFoksha <v.foksh6@gmail.com>
2025-07-29 10:01:23 +0200

    Update database and uv.lock after test on hexlet

--- database.sql
+++ database.sql
@@ -7,13 +7,13 @@ CREATE TABLE urls (
 );
 
 
-  CREATE TABLE checks (
-      id SERIAL PRIMARY KEY,
-      url_id INTEGER NOT NULL,
-      status_code INTEGER,
-      h1 VARCHAR(255),
-      title VARCHAR(255),
-      description TEXT,
-      created_at  TIMESTAMP NOT NULL,
-      FOREIGN KEY (url_id) REFERENCES urls(id) ON DELETE CASCADE
-  );
+CREATE TABLE checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL,
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at  TIMESTAMP NOT NULL,
+    FOREIGN KEY (url_id) REFERENCES urls(id) ON DELETE CASCADE
+);
